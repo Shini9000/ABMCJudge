9bd5884a1286dea9b37922efcb95b8afb43515ae
Aidan Christie <aidanchristie2232@gmail.com>
2024-06-17 13:16:16 +0100

    Update SubmitCommand.java and Version
    Update SubmitCommand.java and Version

--- src/main/java/me/shini9000/abmcjudge/commands/SubmitCommand.java
+++ src/main/java/me/shini9000/abmcjudge/commands/SubmitCommand.java
@@ -1,15 +1,21 @@
 package me.shini9000.abmcjudge.commands;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.jetbrains.annotations.NotNull;
 
 public class SubmitCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String s, String[] strings) {
+
+        if (!(sender instanceof Player)) {
+            Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "This command can only be ran by a player!");
+            return true;
+        }
 
         if (sender instanceof Player) {
             Player p = (Player) sender;
