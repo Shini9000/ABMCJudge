6ae3f9194dd61f5a942c01fcecf1e67251a07f19
Aidan Christie <aidanchristie2232@gmail.com>
2024-06-21 19:47:02 +0100

    Changed some trivial stuff

--- src/main/java/me/shini9000/abmcjudge/commands/MakeJudge.java
+++ src/main/java/me/shini9000/abmcjudge/commands/MakeJudge.java
@@ -14,17 +14,40 @@ public class MakeJudge implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String s, String[] args) {
         Player p = (Player) sender;
-        if (sender instanceof Player){
+        if (args.length == 0) {
+            p.sendMessage(ChatColor.RED + "Incorrect usage please enter a player name /mkjudge <PlayerName>");
+            return false;
+        }
+        if (args.length >= 1) {
             Player tp = Bukkit.getPlayerExact(args[0]);
-            if(!(tp.isOnline())) {
-                p.sendMessage(ChatColor.GOLD + "The player " + tp.getName() + " is offline but will be promoted.");
-                //Promote target player
-            } else {
+            if (args.equals(tp.getName())) {
                 p.sendMessage(ChatColor.GOLD + "Player " + tp.getName() + " was made a judge!");
                 tp.sendMessage(ChatColor.GOLD + "You were promoted to Judge by " + p.getName());
                 // promote target player
+                if (!(tp.isOnline())) {
+                    p.sendMessage(ChatColor.GOLD + "The player " + tp.getName() + " is offline but will be promoted.");
+                    //Promote target player
+                    return true;
+                }
+            } else {
+                p.sendMessage(ChatColor.RED + args.toString() + " is not a player");
+                return false;
             }
+            return false;
         }
+
+
+//        if (sender instanceof Player){
+//            Player tp = Bukkit.getPlayerExact(args[0]);
+//            if(!(tp.isOnline())) {
+//                p.sendMessage(ChatColor.GOLD + "The player " + tp.getName() + " is offline but will be promoted.");
+//                //Promote target player
+//            } else {
+//                p.sendMessage(ChatColor.GOLD + "Player " + tp.getName() + " was made a judge!");
+//                tp.sendMessage(ChatColor.GOLD + "You were promoted to Judge by " + p.getName());
+//                // promote target player
+//            }
+//        }
         return true;
     }
 }
