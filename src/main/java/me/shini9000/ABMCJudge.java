cbfb134e2009fb116b9a443ede052c52c1aa08b3
Aidan Christie <aidanchristie2232@gmail.com>
2024-09-16 15:52:03 +0100

    Update Plugin.yml and Main

--- src/main/java/me/shini9000/ABMCJudge.java
+++ src/main/java/me/shini9000/ABMCJudge.java
@@ -1,11 +1,17 @@
 package me.shini9000;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ABMCJudge extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        Bukkit.getConsoleSender().sendMessage("Enabling plugin: "
+                + getDescription().getName()
+                + " Version: "
+                + getDescription().getVersion()
+        );
         // Plugin startup logic
 
     }
