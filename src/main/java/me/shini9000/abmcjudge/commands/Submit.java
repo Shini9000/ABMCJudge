1d80b48db2f356ff14c7220049099a547d8e181a
Aidan Christie <aidanchristie2232@gmail.com>
2024-09-16 22:34:48 +0100

    1.6 fixes

--- src/main/java/me/shini9000/abmcjudge/commands/Submit.java
+++ src/main/java/me/shini9000/abmcjudge/commands/Submit.java
@@ -4,26 +4,29 @@ import com.plotsquared.bukkit.util.BukkitUtil;
 import com.plotsquared.core.player.PlotPlayer;
 import com.plotsquared.core.plot.PlotId;
 import me.shini9000.abmcjudge.ABMCJudge;
+import me.shini9000.abmcjudge.gui.PlotOverviewMenu;
 import me.shini9000.abmcjudge.utils.PlotUtils;
+import me.shini9000.abmcjudge.utils.SQLUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.PluginDescriptionFile;
 
 public class Submit implements CommandExecutor {
-    private ABMCJudge  plugin;
-    //private final SQLUtils sqlutils = new SQLUtils();
 
-    public Submit(ABMCJudge plugin){
+    private final SQLUtils sqlUtils = new SQLUtils();
+    private ABMCJudge plugin;
+
+    public Submit(ABMCJudge plugin) {
         this.plugin = plugin;
         plugin.getCommand("submit").setExecutor(this);
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
+
         if (!(sender instanceof Player)) { this.plugin.getConfig().getString("Console.error"); return true;}
 
         Player player = (Player) sender;
@@ -33,24 +36,26 @@ public class Submit implements CommandExecutor {
         }
 
         PlotId id = PlotUtils.getId(player);
-        if (id == null) {
-            player.sendMessage(ChatColor.RED + "You must be standing in your plot");
+
+        if(id == null) {
+            player.sendMessage(ChatColor.RED + "You must stand on your plot");
             return true;
         }
 
         PlotPlayer p = BukkitUtil.adapt(player);
+
         boolean isOwner = Bukkit.getOfflinePlayer(p.getCurrentPlot().getOwner()).getName() == player.getName();
+
         if (!isOwner) {
-            player.sendMessage(ChatColor.RED + "You must be standing in your own plot");
+            player.sendMessage(ChatColor.RED + "You must stand on your plot");
             return true;
         }
 
-        //sqlUtils.setPlotTable(player)
-        //sqlUtils.addPlotID(id.toString());
-        //new PlotOverviewMenu(ABMCJudge.getPlayerMenuUtils(player)).open();
+        sqlUtils.setPlotTable(player);
+
+        sqlUtils.addPlotID(id.toString());
+        new PlotOverviewMenu(ABMCJudge.getPlayerMenuUtils(player)).open();
         return true;
-
-
-
     }
+
 }
