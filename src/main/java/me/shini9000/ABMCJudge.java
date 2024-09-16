620e537e06277e6fc4dca77fda96e656f0e2bab1
Aidan Christie <aidanchristie2232@gmail.com>
2024-09-16 15:07:25 +0100

    Build 1 for start

---
+++ src/main/java/me/shini9000/ABMCJudge.java
@@ -0,0 +1,17 @@
+package me.shini9000;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ABMCJudge extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
